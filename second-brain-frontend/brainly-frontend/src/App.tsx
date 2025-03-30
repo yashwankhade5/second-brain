abe2c28c7cef36ccb1a4d3a75a1fca2e0708941f
Yash Wankhade <yashwankhade5@gmail.com>
2025-03-30 05:27:59 -0700

    solved the problems

--- second-brain-frontend/brainly-frontend/src/App.tsx
+++ second-brain-frontend/brainly-frontend/src/App.tsx
@@ -1,12 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
-import { Button } from './components/Button'
+
 import { Dashboard } from './components/Dashboard'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <div>
--- second-brain-frontend/brainly-frontend/src/components/Dashboard.tsx
+++ second-brain-frontend/brainly-frontend/src/components/Dashboard.tsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
+
 
 export function Dashboard() {
-  const [v,setv]=useState()
+ 
 
 
   return (
