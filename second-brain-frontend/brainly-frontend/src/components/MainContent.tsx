e68854259988c17728efce748a1d41ca2403663a
Yash Wankhade <yashwankhade5@gmail.com>
2025-04-01 08:08:51 -0700

    title set on card

--- second-brain-frontend/brainly-frontend/src/components/MainContent.tsx
+++ second-brain-frontend/brainly-frontend/src/components/MainContent.tsx
@@ -4,7 +4,7 @@ import { Bird } from "../icons/bird";
 
 export function Main() {
     return <div className="flex-grow p-9 bg-green-400"> 
-    <Card title={"project"} type={"twitter"}/>
+    <Card title={"Projectwww wwwwwww"} type={"twitter"}/>
     
     </div>
 }
@@ -15,6 +15,6 @@ interface cardType{
 }
 function Card(param:cardType) {
     return <div>
-<div className="flex gap-4 items-center bg-white w-"><div>{<Bird/>}</div><div>{param.title}</div><div><Shareicon/></div><div>{<Dustbin/>}</div></div>
+<div className="flex gap-5  pl-4 rounded-lg items-center max-w-60 p-2 bg-white"><div>{<Bird/>}</div><div className="flex flex-wrap max-w-30">{param.title}</div><div className="text-gray-500 pl-5"><Shareicon/></div><div>{<Dustbin/>}</div></div>
     </div>
 }
