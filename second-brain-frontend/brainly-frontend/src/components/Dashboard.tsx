ccaa0d67c681e3321c4549a5600e1a3ceef66da2
Yash Wankhade <yashwankhade5@gmail.com>
2025-04-01 07:41:19 -0700

    card progress

--- second-brain-frontend/brainly-frontend/src/components/Dashboard.tsx
+++ second-brain-frontend/brainly-frontend/src/components/Dashboard.tsx
@@ -2,6 +2,8 @@
 import { Sidebar } from "./Sidebar";
 import { TopBar } from "./TopBar";
 import { Main } from "./MainContent";
+import { Bird } from "../icons/bird";
+
 
 
 export function Dashboard() {
@@ -10,7 +12,7 @@ export function Dashboard() {
 
   return (<div className="flex">
     <Sidebar/>
-    <div className="w-8/10 min-h-screen flex flex-col "><TopBar/><Main/></div>
+    <div className="w-8/10 min-h-screen flex flex-col "><TopBar/><Main/><Bird/></div>
     
     </div>
   )
--- second-brain-frontend/brainly-frontend/src/components/MainContent.tsx
+++ second-brain-frontend/brainly-frontend/src/components/MainContent.tsx
@@ -1,13 +1,20 @@
+import { Dustbin } from "../icons/Dustbin";
+import { Shareicon } from "../icons/Share";
+import { Bird } from "../icons/bird";
+
 export function Main() {
     return <div className="flex-grow p-9 bg-green-400"> 
-    <Card/>
+    <Card title={"project"} type={"twitter"}/>
     
     </div>
 }
 
-
-function Card() {
+interface cardType{
+    title:string,
+    type:string
+}
+function Card(param:cardType) {
     return <div>
-
+<div className="flex gap-4 items-center bg-white w-"><div>{<Bird/>}</div><div>{param.title}</div><div><Shareicon/></div><div>{<Dustbin/>}</div></div>
     </div>
 }
