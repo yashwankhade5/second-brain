91693597b0ad032f6e128c5bc661946ce40bb1f5
Yash Wankhade <yashwankhade5@gmail.com>
2025-04-02 10:33:26 -0700

    card progress

--- second-brain-frontend/brainly-frontend/src/components/MainContent.tsx
+++ second-brain-frontend/brainly-frontend/src/components/MainContent.tsx
@@ -1,20 +1,31 @@
 import { Dustbin } from "../icons/Dustbin";
 import { Shareicon } from "../icons/Share";
-import { Bird } from "../icons/bird";
+import { Bird } from "../icons/Bird";
 
 export function Main() {
     return <div className="flex-grow p-9 bg-green-400"> 
-    <Card title={"Projectwww wwwwwww"} type={"twitter"}/>
+    <Card title={"Project Idea nameww frgrgg it"} type={"twitter"}/>
     
     </div>
 }
 
 interface cardType{
     title:string,
-    type:string
+    type:"youtube" | "twitter"
 }
 function Card(param:cardType) {
-    return <div>
-<div className="flex gap-5  pl-4 rounded-lg items-center max-w-60 p-2 bg-white"><div>{<Bird/>}</div><div className="flex flex-wrap max-w-30">{param.title}</div><div className="text-gray-500 pl-5"><Shareicon/></div><div>{<Dustbin/>}</div></div>
+    return <div className="max-w-80 p-2  rounded-lg bg-white">
+<div className="flex gap-3  mb-3 rounded-lg items-center max-w-80 p-2 bg-white"><div>{<Bird/>}</div><div className="flex w-70">{param.title}</div><div className="text-gray-500 "><Shareicon/></div><div>{<Dustbin/>}</div></div>
+<iframe
+                            className="w-full"
+                            src={"https://www.youtube.com/embed?v=4XVvbZj794o&t=1357s"}
+                            title="YouTube video player"
+                            frameBorder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                            referrerPolicy="strict-origin-when-cross-origin"
+                            allowFullScreen
+                        ></iframe>
+                        
+
     </div>
 }
