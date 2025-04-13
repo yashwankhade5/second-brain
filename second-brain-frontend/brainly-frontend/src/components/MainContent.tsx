e7f154d2069113118c879ef362207346afe33faf
Yash Wankhade <yashwankhade5@gmail.com>
2025-04-13 15:18:28 +0530

    card components finished

--- second-brain-frontend/brainly-frontend/src/components/MainContent.tsx
+++ second-brain-frontend/brainly-frontend/src/components/MainContent.tsx
@@ -1,39 +1,60 @@
 import { Dustbin } from "../icons/Dustbin";
-import { Shareicon } from "../icons/Share";
+import { ShareIcon } from "../icons/Share";
 import { Bird } from "../icons/Bird";
 
 export function Main() {
     return  <div className="flex-grow items-start flex-wrap flex gap-3 p-9 bg-green-400"> 
-    <Card title={"Project Idea nameww frgrgg it"} type={"twitter"}/>
-    <Card title={"Project Idea nameww frgrgg it"} type={"twitter"}/>
-    <Card title={"Project Idea nameww frgrgg it"} type={"twitter"}/>
-    <Card title={"Project Idea nameww frgrgg it"} type={"twitter"}/>
-    <Card title={"Project Idea nameww frgrgg it"} type={"twitter"}/>
-    <Card title={"Project Idea nameww frgrgg it"} type={"twitter"}/>
-    <Card title={"Project Idea nameww frgrgg it"} type={"twitter"}/>
-   
+    <Card title={"Project Idea nameww frgrgg it"} link={"https://www.youtube.com/watch?v=8_uNrBN3f2Q"} type={"youtube"}/>
+    <Card title={"Project Idea nameww frgrgg it"} link={"https://twitter.com/tweet/status/1903543461934235680?ref_src=twsrc%5Etfw"} type={"twitter"}/>
+    <Card title={"Project Idea nameww frgrgg it"} link={"https://twitter.com/tweet/status/1903543461934235680?ref_src=twsrc%5Etfw"} type={"twitter"}/>
+    <Card title={"Project Idea nameww frgrgg it"} link={"https://twitter.com/tweet/status/1903543461934235680?ref_src=twsrc%5Etfw"} type={"twitter"}/>
+    <Card title={"Project Idea nameww frgrgg it"} link={"https://twitter.com/tweet/status/1903543461934235680?ref_src=twsrc%5Etfw"} type={"twitter"}/>
+    <Card title={"Project Idea nameww frgrgg it"} link={"https://twitter.com/tweet/status/1903543461934235680?ref_src=twsrc%5Etfw"} type={"twitter"}/>
+
+    
     
     
     </div>
 }
 
-interface cardType{
-    title:string,
-    type:"youtube" | "twitter"
-}
-function Card(param:cardType) {
-    return <div className="max-w-80 p-2 pb-9 h-auto rounded-lg bg-white">
-<div className="flex gap-3  mb-3 rounded-lg items-center max-w-80 p-2 bg-white"><div>{<Bird/>}</div><div className="flex w-70">{param.title}</div><div className="text-gray-500 "><Shareicon/></div><div>{<Dustbin/>}</div></div>
-<iframe
-                            className="w-full"
-                            src={"https://www.youtube.com/embed/DDo_RPrYZH0?si=7XdjBZs1jWZUU9Iv"}
-                            title="YouTube video player"
-                            frameBorder="0"
-                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                            referrerPolicy="strict-origin-when-cross-origin"
-                            allowFullScreen
-                        ></iframe>
-                        
 
+
+interface CardProps {
+    title: string;
+    link: string;
+    type: "twitter" | "youtube";
+}
+
+export function Card({title, link, type}: CardProps) {
+    return <div>
+        <div className="p-4 bg-white rounded-md border-gray-200 max-w-72  border min-h-48 min-w-72">
+            <div className="flex justify-between">
+                <div className="flex items-center text-md">
+                    <div className="text-gray-500 pr-2">
+                        <ShareIcon />
+                    </div>
+                    {title}
+                </div>
+                <div className="flex items-center">
+                    <div className="pr-2 text-gray-500">
+                        <a href={link} target="_blank">
+                            <ShareIcon />
+                        </a>
+                    </div>
+                    <div className="text-gray-500">
+                        <ShareIcon />
+                    </div>
+                </div>
+            </div>
+
+            <div className="pt-4">
+                {type === "youtube" && <iframe className="w-full" src={link.replace("watch", "embed").replace("?v=", "/")} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>}
+
+                {type === "twitter" && <blockquote className="twitter-tweet"><p lang="en" dir="ltr">&quot;Make comedy legal again. Yeah!&quot; - Elon Musk, March 2025 <a href={link.replace("x.com", "twitter.com")}></a></p>&mdash; Tweet (@tweet) <a href="https://twitter.com/tweet/status/1903543461934235680?ref_src=twsrc%5Etfw">March 22, 2025</a> <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
+                     
+                </blockquote>}
+            </div>
+
+        </div>
     </div>
 }
--- second-brain-frontend/brainly-frontend/src/components/TopBar.tsx
+++ second-brain-frontend/brainly-frontend/src/components/TopBar.tsx
@@ -1,13 +1,13 @@
 import  {Button}  from "./Button";
 import { PlusIcon } from "../icons/PlusIcon";
-import { Shareicon } from "../icons/Share";
+import { ShareIcon } from "../icons/Share";
 
 
 export function TopBar() {
     return<div className=" px-5 bg-blue-100"><div className="flex  justify-between p-5 ">
     <div className="text-2xl font-bold">All Notes</div>
     <div className="flex ">
-      <Button title={"Share Brain"} variant={"primary"} size={"lg"} starticon={<Shareicon/>}/>
+      <Button title={"Share Brain"} variant={"primary"} size={"lg"} starticon={<ShareIcon/>}/>
       <Button title={"Add Content"} variant={"secondary"} size={"lg"} starticon={<PlusIcon/>}/>
     </div>
     </div></div>
